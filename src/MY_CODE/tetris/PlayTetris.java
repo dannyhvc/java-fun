e71b57e1595d0b0dd93029a108ee84cc1df3b80d
Daniel Herrera <26307270+dannyhvc@users.noreply.github.com>
2019-02-25 14:54:46 -0500

    added a tetris game in MYCODE

---
+++ src/MY_CODE/tetris/PlayTetris.java
@@ -0,0 +1,27 @@
+/*
+ * Project Name: PlayTetris.java
+ * Purpose:
+ * Coder: Daniel Herrera (0881570) for Section 03
+ * Date: 2/25/2019 | Time: 2:34 PM
+ */
+package MY_CODE.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayTetris extends JFrame
+{
+  public PlayTetris()
+  {
+    getContentPane().setLayout(new FlowLayout());
+    add(new TetrisComponent(15,30));
+    pack();
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
+  public static void main(String[] args)
+  {
+    new PlayTetris();
+  }
+}
